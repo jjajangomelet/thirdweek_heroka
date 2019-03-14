209f2b3734c3c29da5f55cef7fc606e19425ad8f
jjajangomelet <35620908+jjajangomelet@users.noreply.github.com>
2019-03-14 12:22:55 +0900

    first cmmit of third

---
+++ blogapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Blog
+
+# Create your views here.
+
+def home(request):
+    blogs = Blog.objects #쿼리셋
+    return render(request, 'home.html', {'blogs' : blogs})
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk = blog_id)
+
+    return render(request, 'detail.html', {'blog' : blog_detail})
+
+
+def new(request): #new 띄우는 함수
+    return render(request, 'new.html')
+
+def create(request): #입력받은 내용을 데이터베이스에 넣어주는 함수   
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+
+    return redirect('/blog/'+str(blog.id))
+#쿼리셋과 메소드의 형식
+# 모델.쿼리셋(objects).메소드
+    
